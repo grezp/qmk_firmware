827d607a329eefef2a7faf706111d4c54de7e962
pat <pat@Pats-Mac.local>
2020-10-14 09:58:25 -0700

    added grezp keymap to orig qmk repo

---
+++ keyboards/crkbd/keymaps/grezp/enums.h
@@ -0,0 +1,10 @@
+// [CRKBD layers Init] -------------------------------------------------------//
+typedef enum {
+    _COLEMAK,
+    _QWERTY,
+    _NUM,
+    _SYM,
+    _ADJ
+}CRKBD_LAYERS;
+
+extern enum CRKBD_LAYERS crkbd_layers;
